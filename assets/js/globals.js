74fe75d800b0ece83b6cd4c88f0abc4b989b9aaf
LeeFuuChang <77952182+LeeFuuChang@users.noreply.github.com>
2023-08-19 01:23:28 +0800

    Delete assets directory

--- assets/js/globals.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(element => {
-    element.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(element.getAttribute('href')).scrollIntoView({
-            behavior: "smooth"
-        });
-    });
-});
