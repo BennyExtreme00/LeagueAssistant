b5a3db5c414aa44f963c31be986b9c0d2b904e1a
LeeFuuChang <77952182+LeeFuuChang@users.noreply.github.com>
2023-08-19 11:01:14 +0800

    Add files via upload

--- src/LeagueAssistant-LS/fe/assets/overlay/about-info/class.js
+++ src/LeagueAssistant-LS/fe/assets/overlay/about-info/class.js
@@ -11,22 +11,20 @@ class AppOverlay_AboutInfo extends AppOverlay {
         $(this.element).find(".container .content").each(function(){
             $(this).toggle($(this).attr("data-name") === active);
         });
-        if(active === "version"){
-            $.get("/app/version", {}, (appVdata)=>{
-                $.get("/storage/local/storage.version", {}, (data)=>{
-                    let feV = parseInt(data, 16);
-                    $(this.element)
-                    .find(".content[data-name='version'] .group[data-name='current'] p")
-                    .text(`${appVdata["current"]}.${feV}`);
-                });
-                $.get("/storage/cloud/struct.xml", {}, (data)=>{
-                    let feV = parseInt(data.documentElement.getAttribute("version"), 16);
-                    $(this.element)
-                    .find(".content[data-name='version'] .group[data-name='latest'] p")
-                    .text(`${appVdata["latest"]}.${feV}`);
-                });
-                $(this.element).find(".content[data-name='version'] .group[data-name='update'] p").text(appVdata["update"]);
+        $.get("/app/version", {}, (appVdata)=>{
+            $.get("/storage/local/storage.version", {}, (data)=>{
+                let feV = parseInt(data, 16);
+                $(this.element)
+                .find(".content[data-name='version'] .group[data-name='current'] p")
+                .text(`${appVdata["current"]}.${feV}`);
             });
-        }
+            $.get("/storage/cloud/struct.xml", {}, (data)=>{
+                let feV = parseInt(data.documentElement.getAttribute("version"), 16);
+                $(this.element)
+                .find(".content[data-name='version'] .group[data-name='latest'] p")
+                .text(`${appVdata["latest"]}.${feV}`);
+            });
+            $(this.element).find(".content[data-name='version'] .group[data-name='update'] p").text(appVdata["update"]);
+        });
     }
 }
